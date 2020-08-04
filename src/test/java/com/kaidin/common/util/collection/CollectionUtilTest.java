beb243c8f72a4ad772a0e32ac00ab3bb76679009
徐小斌 <kaidin@foxmail.com>
2020-08-04 10:12:28 +0800

    去掉eclipse配置

---
+++ src/test/java/com/kaidin/common/util/collection/CollectionUtilTest.java
@@ -0,0 +1,52 @@
+package com.kaidin.common.util.collection;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CollectionUtilTest {
+
+    @Test
+    public void isEmpty() {
+    }
+
+    @Test
+    public void isNotEmpty() {
+    }
+
+    @Test
+    public void testIsEmpty() {
+    }
+
+    @Test
+    public void testIsNotEmpty() {
+    }
+
+    @Test
+    public void testIsEmpty1() {
+    }
+
+    @Test
+    public void testIsNotEmpty1() {
+    }
+
+    @Test
+    public void asArray() {
+    }
+
+    @Test
+    public void asArrayList() {
+    }
+
+    @Test
+    public void subList() {
+    }
+
+    @Test
+    public void testSubList() {
+    }
+
+    @Test
+    public void decode() {
+    }
+}
